ea022962b3f48c79fb49cf82dcf6814d0c332890
Joynal Abedin <joynal3483@gmail.com>
2019-02-07 02:49:20 +0600

    fake data init

---
+++ utils/random.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"crypto/rand"
+)
+
+// GenerateRandomBytes will generate random bytes
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	// Note that err == nil only if we read len(b) bytes.
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// GenerateRandomString will generate random string
+func GenerateRandomString(n int) (string, error) {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
+	bytes, err := GenerateRandomBytes(n)
+	if err != nil {
+		return "", err
+	}
+	for i, b := range bytes {
+		bytes[i] = letters[b%byte(len(letters))]
+	}
+	return string(bytes), nil
+}
