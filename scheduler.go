cced20ba3b73fb9c2a4e2c649fe3817957af3688
Joynal Abedin <joynal3483@gmail.com>
2019-02-14 09:19:08 +0600

    query added

--- scheduler.go
+++ scheduler.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
-
 	"pushservice/models"
 
 	"github.com/globalsign/mgo/bson"
 	"github.com/mongodb/mongo-go-driver/bson/primitive"
 	"github.com/mongodb/mongo-go-driver/mongo"
+	"github.com/mongodb/mongo-go-driver/mongo/options"
 )
 
 func main() {
@@ -22,15 +22,48 @@ func main() {
 
 	fmt.Println("Connected to MongoDB!")
 
-	siteId, _ := primitive.ObjectIDFromHex("5c5b9ee1752a41a63268c3bb")
+	siteID, _ := primitive.ObjectIDFromHex("5c5b9ee1752a41a63268c3bb")
 
-	var site models.Site
-	idDoc := bson.D{{"_id", siteId}}
-	siteCollection := client.Database("pushservice").Collection("sites")
-	err = siteCollection.FindOne(context.TODO(), idDoc).Decode(&site)
+	// var site models.Site
+	// filter := bson.D{{"_id", siteID}}
+
+	// siteCollection := client.Database("pushservice").Collection("sites")
+	// err = siteCollection.FindOne(context.TODO(), filter).Decode(&site)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Printf("Found a single document: %+v\n", site)
+
+	// Finding multiple documents returns a cursor
+	notificationCollection := client.Database("pushservice").Collection("notifications")
+	findOptions := options.Find()
+	findOptions.SetLimit(2)
+
+	var results []*models.Site
+
+	cur, err := notificationCollection.Find(context.TODO(), bson.D{{"siteId", siteID}}, findOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Found a single document: %+v\n", site)
+	// Iterate through the cursor
+	for cur.Next(context.TODO()) {
+		var elem models.Site
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results = append(results, &elem)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Close the cursor once finished
+	cur.Close(context.TODO())
+	fmt.Printf("Found multiple documents (array of pointers): %+v\n", results)
 }
