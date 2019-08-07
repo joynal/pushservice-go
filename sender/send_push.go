a1cf8c392d4866b06e64a871c622771529de48ff
Joynal Abedin <joynal3483@gmail.com>
2019-08-07 10:32:01 +0600

    added logging

--- sender/send_push.go
+++ sender/send_push.go
@@ -1,26 +1,29 @@
 package parser
 
 import (
-  "encoding/json"
-  "github.com/Shopify/sarama"
-  "log"
-  "sync"
+	"encoding/json"
+	"fmt"
+	"github.com/Shopify/sarama"
+	"log"
+	"sync"
 
-  "pushservice-go/models"
+	"pushservice-go/models"
 )
 
-func sendPush(msg *sarama.ConsumerMessage, sess sarama.ConsumerGroupSession, maxChan chan bool, wg *sync.WaitGroup)  {
-  wg.Add(1)
-  defer wg.Done()
-  defer func(maxChan chan bool) { <-maxChan }(maxChan)
+func sendPush(msg *sarama.ConsumerMessage, sess sarama.ConsumerGroupSession, maxChan chan bool, wg *sync.WaitGroup) {
+	wg.Add(1)
+	defer wg.Done()
+	defer func(maxChan chan bool) { <-maxChan }(maxChan)
 
-  // construct struct from byte
-  var push models.PushPayload
-  err := json.Unmarshal(msg.Value, &push)
-  if err != nil {
-    log.Fatal(err)
-  }
+	// construct struct from byte
+	var push models.PushPayload
+	err := json.Unmarshal(msg.Value, &push)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  // commit kafka message
-  sess.MarkMessage(msg, "")
+	fmt.Println("record: ", string(msg.Value))
+
+	// commit kafka message
+	sess.MarkMessage(msg, "")
 }
