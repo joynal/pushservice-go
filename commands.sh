cb4f2ff8e83c0200ef7e92ada708c64f7afe3da3
Joynal Abedin <joynal3483@gmail.com>
2019-08-24 16:42:14 +0600

    task added

--- commands.sh
+++ commands.sh
@@ -2,15 +2,15 @@
 sudo apt update && sudo apt upgrade && sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt install -y make python git vim curl wget g++ libsasl2-dev libssl-dev libstdc++6
-ssh-keygen -t rsa -b 4096 -C “joynal.uu@gmail.com”
-cat .ssh/id_rsa.pub
 sudo snap install go --classic
 mkdir -p /home/joynal3483/go/bin
 curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-source .profile
+ssh-keygen -t rsa -b 4096 -C “joynal.uu@gmail.com”
+cat .ssh/id_rsa.pub
 
 # copy this path
 # PATH=$PATH":/home/joynal3483/go/bin"
+source .profile
 
 mkdir -p go/src && cd go/src/
 git clone git@bitbucket.org:Joynal/pushservice-go.git
@@ -19,4 +19,5 @@ cp .env.example .env
 dep ensure
 
 # server tewak
-ulimit -n 2000000
+ulimit -n 1000000
+ulimit -S 1000000
